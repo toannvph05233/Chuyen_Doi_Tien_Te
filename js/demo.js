374402e11e04e657e772ffb689b7f0b7852d09e1
toannvph05233 <johntoan98@gmail.com>
2023-04-05 17:29:40 +0700

    upp

---
+++ js/demo.js
@@ -0,0 +1,25 @@
+/*
+
+ tạo 1 hàm
+ khi tạo hàm, thì code sẽ không chạy.
+ Code chạy khi hàm được gọi.
+
+ */
+function demoPrompt(){
+    let a,b;
+    a = prompt('nhập số a');
+    b = prompt("nhập số b");
+    alert(a*b);
+}
+
+function demoAlert(){
+    alert("xin chào c0322h1, đây là alert")
+}
+function demoConfirm(){
+    confirm("Bạn muốn xóa chứ")
+}
+
+function demoWrite(){
+    document.write("<h1>Hello C0323h1</h1>")
+}
+
